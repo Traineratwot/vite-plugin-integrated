ae47f09c5fbe1bb363617eedb7e29a822b8379fa
Kirill <traineratwot@yandex.ru>
2023-06-16 13:30:06 +0300

    FIX

--- plugin/index.cjs
+++ plugin/index.cjs
@@ -6,22 +6,22 @@ import jsdom from 'jsdom'
 let outDir = ''
 
 function parser(html) {
-	const doc = new jsdom.JSDOM(html).window.document
-	const scripts = []
-	const styles = []
-	doc.querySelectorAll('script').forEach(script => scripts.push(script.getAttribute('src')))
-	doc.querySelectorAll('link').forEach(
-		style => {
-			if(style.getAttribute('rel') === 'stylesheet') {
-				styles.push(style.getAttribute('href'))
-			}
-		}
-	)
+    const doc = new jsdom.JSDOM(html).window.document
+    const scripts = []
+    const styles = []
+    doc.querySelectorAll('script').forEach(script => scripts.push(script.getAttribute('src')))
+    doc.querySelectorAll('link').forEach(
+        style => {
+            if (style.getAttribute('rel') === 'stylesheet') {
+                styles.push(style.getAttribute('href'))
+            }
+        }
+    )
 
-	return {
-		scripts,
-		styles
-	}
+    return {
+        scripts,
+        styles
+    }
 }
 
 /**
@@ -29,24 +29,26 @@ function parser(html) {
  * @param {{templatePath: string,name?: string,	options?: { [key: string]: any }}} config
  */
 export const PluginIntegrated = (config) => ({
-	name  : 'vite-integrated-plugin',
-	outDir: '',
-	configResolved(config) {
-		outDir = config.build.outDir
-	},
-	async transformIndexHtml(html, ctx) {
-		const options = parser(html)
+    name: 'vite-integrated-plugin',
+    outDir: '',
+    configResolved(config) {
+        outDir = config.build.outDir
+    },
+    async transformIndexHtml(html, ctx) {
+        const options = parser(html)
 
-		const newHtml = await ejs.renderFile(config.templatePath, {...config.options, ...options,})
-		if(config.name) {
-			const newFileName = path.join(outDir, config.name)
-			console.log(newFileName)
-			fs.writeFileSync(newFileName, newHtml)
-			return html
-		} else {
-			return newHtml
-		}
-	},
+        const newHtml = await ejs.renderFile(config.templatePath, {...config.options, ...options,})
+        if (fs.existsSync(outDir)) {
+            if (config.name) {
+                const newFileName = path.join(outDir, config.name)
+                console.log(newFileName)
+                fs.writeFileSync(newFileName, newHtml)
+                return html
+            } else {
+                return newHtml
+            }
+        }
+    },
 
 })
 
