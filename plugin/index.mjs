534295805014891a611c7dc78ed1a1e89933640e
Kirill <traineratwot@yandex.ru>
2023-06-16 13:27:32 +0300

    Revert "1"
    This reverts commit 5506f247fa6dc55140a48d4eb3def0fecaac78d3.

--- plugin/index.mjs
+++ plugin/index.mjs
@@ -38,7 +38,7 @@ export const PluginIntegrated = (config) => ({
 		const options = parser(html)
 
 		const newHtml = await ejs.renderFile(config.templatePath, {...config.options, ...options,})
-		if(fs.existsSync(outDir) && config.name) {
+		if(config.name) {
 			const newFileName = path.join(outDir, config.name)
 			console.log(newFileName)
 			fs.writeFileSync(newFileName, newHtml)
