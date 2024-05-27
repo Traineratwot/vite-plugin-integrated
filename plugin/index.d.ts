58767d64fd9969e8cb6aeb978254494df8cae899
Kirill <traineratwot@yandex.ru>
2024-05-27 18:24:19 +0300

    outDir

--- plugin/index.d.ts
+++ plugin/index.d.ts
@@ -1,11 +1,13 @@
-import {Plugin} from 'vite';
+import { Plugin } from 'vite';
 
 export type PluginIntegratedOptions = {
 	templatePath: string
 	name?: string
+	outDir?: string
 	options?: { [key: string]: any }
 }
 
 declare function PluginIntegrated(options: PluginIntegratedOptions,): Plugin
 
-export {PluginIntegrated as default};
+export { PluginIntegrated as default };
+
--- plugin/index.js
+++ plugin/index.js
@@ -1,55 +1,50 @@
-import ejs from 'ejs'
-import * as fs from 'node:fs'
-import * as path from 'node:path'
-import jsdom from 'jsdom'
-
-let outDir = ''
+import ejs from "ejs";
+import jsdom from "jsdom";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 function parser(html) {
-    const doc = new jsdom.JSDOM(html).window.document
-    const scripts = []
-    const styles = []
-    doc.querySelectorAll('script').forEach(script => scripts.push(script.getAttribute('src')))
-    doc.querySelectorAll('link').forEach(
-        style => {
-            if (style.getAttribute('rel') === 'stylesheet') {
-                styles.push(style.getAttribute('href'))
-            }
-        }
-    )
+	const doc = new jsdom.JSDOM(html).window.document;
+	const scripts = [];
+	const styles = [];
+	doc.querySelectorAll("script").forEach((script) => scripts.push(script.getAttribute("src")));
+	doc.querySelectorAll("link").forEach((style) => {
+		if (style.getAttribute("rel") === "stylesheet") {
+			styles.push(style.getAttribute("href"));
+		}
+	});
 
-    return {
-        scripts,
-        styles
-    }
+	return {
+		scripts,
+		styles,
+	};
 }
 
 /**
  * Конструктор для создания книги
- * @param {{templatePath: string,name?: string,	options?: { [key: string]: any }}} config
+ * @param {{templatePath: string, outDir:string ,name?: string,	options?: { [key: string]: any }}} myConfig
  */
-export const PluginIntegrated = (config) => ({
-    name: 'vite-integrated-plugin',
-    outDir: '',
-    configResolved(config) {
-        outDir = config.build.outDir
-    },
-    async transformIndexHtml(html, ctx) {
-        const options = parser(html)
+export const PluginIntegrated = (myConfig) => ({
+	name: "vite-integrated-plugin",
+	outDir: "",
+	configResolved(config) {
+		this.outDir = config.build.outDir;
+	},
+	async transformIndexHtml(html, ctx) {
+		const options = parser(html);
+        this.outDir = this.outDir ?? myConfig.outDir
+		const newHtml = await ejs.renderFile(myConfig.templatePath, { ...myConfig.options, ...options });
+		if (fs.existsSync(this.outDir)) {
+			if (myConfig.name) {
+				const newFileName = path.join(this.outDir, myConfig.name);
+				fs.writeFileSync(newFileName, newHtml);
+				console.log("[vite-plugin-integrated]: ", "created", newFileName);
+				return html;
+			} else {
+				return newHtml;
+			}
+		}
+	},
+});
 
-        const newHtml = await ejs.renderFile(config.templatePath, {...config.options, ...options,})
-        if (fs.existsSync(outDir)) {
-            if (config.name) {
-                const newFileName = path.join(outDir, config.name)
-                console.log(newFileName)
-                fs.writeFileSync(newFileName, newHtml)
-                return html
-            } else {
-                return newHtml
-            }
-        }
-    },
-
-})
-
-export default PluginIntegrated
+export default PluginIntegrated;
